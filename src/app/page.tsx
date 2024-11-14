6ae4e96bf323e965c46da0ad11690ac230090c12
yichien <kevinyichienlee@gmail.com>
2024-11-14 23:49:36 +0800

    add a quick signin signout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import { Button } from '@nextui-org/react';
+import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me!!!</Button>
-    </div>
+    <>
+      <form action={actions.signIn}>
+        <Button type="submit">signIn!!!</Button>
+      </form>
+      <form action={actions.signOut}>
+        <Button type="submit">signOut!!!</Button>
+      </form>
+    </>
   );
 }
