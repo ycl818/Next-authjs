aed0883b977df556d29ed5861c2cfd03ec65ab95
yichien <kevinyichienlee@gmail.com>
2024-11-17 23:42:36 +0800

    zod and error msg

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,26 @@
 "use server";
 
-export async function createTopic() {
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Only lowercase letters and hyphens are allowed",
+    }),
+  description: z.string().min(10).max(1000),
+});
+
+export async function createTopic(formData: FormData) {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+  }
+
   // TODO: revalidate the homepage
 }
