dd9b4d058abce7e86fdd526f25e6fcb50958022c
yichien <kevinyichienlee@gmail.com>
2024-11-18 22:55:01 +0800

    displaying validation errors

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -29,12 +29,16 @@ export default function TopicCreateForm() {
               label="Name"
               labelPlacement="outside"
               placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(", ")}
             />
             <Textarea
               name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe yout topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(", ")}
             />
             <Button type="submit">Submit</Button>
           </div>
