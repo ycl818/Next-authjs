c9366fcc20ceb1aa88c3064ae8295757c99caeca
yichien <kevinyichienlee@gmail.com>
2024-11-18 12:02:14 +0800

    fixing useformState Type errors

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -12,7 +12,7 @@ const createTopicSchema = z.object({
   description: z.string().min(10).max(1000),
 });
 
-export async function createTopic(formData: FormData) {
+export async function createTopic(formState: number, formData: FormData) {
   const result = createTopicSchema.safeParse({
     name: formData.get("name"),
     description: formData.get("description"),
@@ -21,6 +21,6 @@ export async function createTopic(formData: FormData) {
   if (!result.success) {
     console.log(result.error.flatten().fieldErrors);
   }
-
+  return 10;
   // TODO: revalidate the homepage
 }
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Button,
   Input,
@@ -7,15 +9,17 @@ import {
   PopoverContent,
 } from "@nextui-org/react";
 import * as actions from "@/actions";
+import { useFormState } from "react-dom";
 
 export default function TopicCreateForm() {
+  const [formState, action] = useFormState(actions.createTopic, 5);
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button>Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
             <Input
