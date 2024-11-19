4f2df27c162e46f1ee7cc41fff99b2a2511f2c90
yichien <kevinyichienlee@gmail.com>
2024-11-19 23:33:45 +0800

    pass args into form and create a post with userId and topicid

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -21,6 +21,7 @@ interface CreatePostFormState {
 }
 
 export async function createPost(
+  slug: string,
   formState: CreatePostFormState,
   formData: FormData
 ): Promise<CreatePostFormState> {
@@ -44,9 +45,48 @@ export async function createPost(
     };
   }
 
-  return {
-    errors: {},
-  };
+  const topic = await db.topic.findFirst({
+    where: {
+      slug,
+    },
+  });
+
+  if (!topic) {
+    return {
+      errors: {
+        _form: ["Topic not found"],
+      },
+    };
+  }
+
+  let post: Post;
+  try {
+    post = await db.post.create({
+      data: {
+        title: result.data.title,
+        content: result.data.content,
+        topicId: topic.id,
+        userId: session.user.id,
+      },
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["An unknown error occurred"],
+        },
+      };
+    }
+  }
+
+  revalidatePath(paths.topicShow(slug));
+  redirect(paths.postShow(slug, post.id));
 
   // TODO: revalidate the topic show page
 }
--- src/components/posts/post-create-form.tsx
+++ src/components/posts/post-create-form.tsx
@@ -11,10 +11,17 @@ import {
 import * as actions from "@/actions";
 import FormButton from "../common/form-button";
 
-export default function PostCreateForm() {
-  const [formState, action] = useFormState(actions.createPost, {
-    errors: {},
-  });
+interface PostCreateFormProps {
+  slug: string;
+}
+
+export default function PostCreateForm({ slug }: PostCreateFormProps) {
+  const [formState, action] = useFormState(
+    actions.createPost.bind(null, slug),
+    {
+      errors: {},
+    }
+  );
 
   return (
     <Popover placement="left">
