799d924d6b99eca60ad6ceb8efc944e99010f875
yichien <kevinyichienlee@gmail.com>
2024-11-19 23:09:40 +0800

    apply validation on create-post

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,5 +1,43 @@
 "use server";
+import type { Post } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
+import { z } from "zod";
+import { db } from "@/db";
+import paths from "@/paths";
+import { revalidatePath } from "next/cache";
 
-export async function createPost() {
+const createPostSchema = z.object({
+  title: z.string().min(3).max(100),
+  content: z.string().min(10).max(100),
+});
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createPost(
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const result = createPostSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  return {
+    errors: {},
+  };
+
   // TODO: revalidate the topic show page
 }
